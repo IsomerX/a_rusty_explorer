e5766e182e0b45b61ab7f617fa60ea1befea90cf
korigamik-hypr <korigamik@gmail.com>
2023-07-22 15:17:18 +0530

    fix(linux): working builds fix(formatting): use 2 space tabs

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,20 @@ import Files from "./Files";
 import "react-tooltip/dist/react-tooltip.css";
 
 export default function Home() {
-    const [currentFolderPath, setCurrentFolderPath] = useState<string>("");
-    return (
-        <div className="bg-[#08090d] h-screen w-screen p-6 overflow-x-hidden">
-            <div className="xl:max-w-4xl mx-auto">
-                <Volumes setDirectory={setCurrentFolderPath} />
-                <Folders
-                    path={currentFolderPath}
-                    setPath={setCurrentFolderPath}
-                />
-                <Files
-                    path={currentFolderPath}
-                    setPath={setCurrentFolderPath}
-                />
-            </div>
-        </div>
-    );
+  const [currentFolderPath, setCurrentFolderPath] = useState<string>(".");
+  return (
+    <div className="bg-[#08090d] h-screen w-screen p-6 overflow-x-hidden">
+      <div className="xl:max-w-4xl mx-auto">
+        <Volumes setDirectory={setCurrentFolderPath} />
+        <Folders
+          path={currentFolderPath}
+          setPath={setCurrentFolderPath}
+        />
+        <Files
+          path={currentFolderPath}
+          setPath={setCurrentFolderPath}
+        />
+      </div>
+    </div>
+  );
 }
